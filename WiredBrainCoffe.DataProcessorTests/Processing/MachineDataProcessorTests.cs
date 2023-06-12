cd79e1aa479c5bf6cc1f3dc611bc5ddcd8c6f917
Krystian Szych <krystian.szych44@gmail.com>
2023-06-12 10:36:08 +0200

    Add project files.

---
+++ WiredBrainCoffe.DataProcessorTests/Processing/MachineDataProcessorTests.cs
@@ -0,0 +1,108 @@
+﻿using WiredBrainCoffee.DataProcessor.Data;
+using WiredBrainCoffee.DataProcessor.Model;
+using WiredBrainCoffee.DataProcessor.Processing;
+
+namespace WiredBrainCoffe.DataProcessor.Processing;
+
+public class MachineDataProcessorTests : IDisposable
+{
+    private readonly ExampleCoffeCountStore _coffeCountStore;
+    private readonly MachineDataProcessor _machineDataProcessor;
+
+    public MachineDataProcessorTests()
+    {
+       _coffeCountStore = new ExampleCoffeCountStore();
+       _machineDataProcessor = new MachineDataProcessor(_coffeCountStore);
+    }
+
+    [Fact]
+    public void ShouldSaveCountPerCoffeeType()
+    {
+        // Arrange  
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0)),
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,9,0,0)),
+            new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0))
+        };
+
+        // Act 
+        _machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, _coffeCountStore.SavedItems.Count);
+
+        var item = _coffeCountStore.SavedItems[0];
+        Assert.Equal("Cappuccino", item.CoffeeType);
+        Assert.Equal(2, item.Count);
+
+        item = _coffeCountStore.SavedItems[1];
+        Assert.Equal("Espresso", item.CoffeeType);
+        Assert.Equal(1, item.Count);
+    }
+
+    [Fact]
+    public void ShouldClearPreviousCoffeCount()
+    {
+        // Arrange
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0))
+        };
+
+        // Act 
+        _machineDataProcessor.ProcessItems(items);
+        _machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, _coffeCountStore.SavedItems.Count);
+        foreach (var item in _coffeCountStore.SavedItems)
+        {
+            Assert.Equal("Cappuccino", item.CoffeeType);
+            Assert.Equal(1, item.Count);
+        }
+    }
+
+    [Fact]
+    public void ShouldIgnoreItemsThatAreNotNewer()
+    {
+        // Arrange  
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0)),
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,7,0,0)),
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,7,10,0)),
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,9,0,0)),
+            new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0)),
+            new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0))
+        };
+
+        // Act 
+        _machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, _coffeCountStore.SavedItems.Count);
+
+        var item = _coffeCountStore.SavedItems[0];
+        Assert.Equal("Cappuccino", item.CoffeeType);
+        Assert.Equal(2, item.Count);
+
+        item = _coffeCountStore.SavedItems[1];
+        Assert.Equal("Espresso", item.CoffeeType);
+        Assert.Equal(1, item.Count);
+    }
+
+    public void Dispose()
+    {
+        // This runs after every test
+    }
+}
+
+public class ExampleCoffeCountStore : ICoffeCountStore
+{
+    public List<CoffeCountItem> SavedItems { get; } = new();
+    public void Save(CoffeCountItem item)
+    {
+        SavedItems.Add(item);
+    }
+}
---
+++ WiredBrainCoffee.DataProcessor/Data/ConsoleCoffeCountStore.cs
@@ -0,0 +1,22 @@
+﻿using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Data
+{
+    public class ConsoleCoffeCountStore : ICoffeCountStore
+    {
+        private readonly TextWriter _testWriter;
+
+        public ConsoleCoffeCountStore() : this(Console.Out) { }
+
+        public ConsoleCoffeCountStore(TextWriter textWriter)
+        {
+            _testWriter = textWriter;
+        }
+
+        public void Save(CoffeCountItem item)
+        {
+            var line = $"{item.CoffeeType}:{item.Count}";
+            _testWriter.WriteLine(line); ;
+        }
+    }
+}
